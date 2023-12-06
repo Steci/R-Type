907e649e2bac0c461790ddaba854ce05d41cd464
Guillaume CLEMENT--BONNIEL-VEYRON <guillaume.clement-bonniel-veyron@epitech.eu>
2023-12-06 09:51:09 +0100

    [ADD] Basic Sparse Array implementation

--- engine/include/Engine.hpp
+++ engine/include/Engine.hpp
@@ -7,9 +7,64 @@
 
 #pragma once
 
+#include <vector>
+#include <cassert>
+
 #include "Entity.hpp"
 #include "Component.hpp"
 #include "System.hpp"
 
 // Sparse Array
+template<typename T>
+class SparseArray {
+    public:
+        void add(int id, const T& element) {
+            if (id >= sparse.size()) {
+                sparse.resize(id + 1, -1);
+            }
 
+            if (sparse[id] == -1) {
+                sparse[id] = dense.size();
+                dense.push_back(element);
+                indices.push_back(id);
+            } else {
+                // Replace if already exists
+                dense[sparse[id]] = element;
+            }
+        }
+
+        void remove(int id) {
+            assert(id < sparse.size() && sparse[id] != -1 && "Invalid ID");
+
+            int denseIndex = sparse[id];
+            int lastIndex = dense.size() - 1;
+            int lastID = indices[lastIndex];
+
+            std::swap(dense[denseIndex], dense[lastIndex]);
+            std::swap(indices[denseIndex], indices[lastIndex]);
+
+            sparse[lastID] = denseIndex;
+            sparse[id] = -1;
+
+            dense.pop_back();
+            indices.pop_back();
+        }
+
+        T& get(int id) {
+            assert(id < sparse.size() && sparse[id] != -1 && "Invalid ID");
+            return dense[sparse[id]];
+        }
+
+        bool exists(int id) const {
+            return id < sparse.size() && sparse[id] != -1;
+        }
+
+    private:
+        std::vector<T> dense; // Stores actual elements
+        std::vector<int> sparse; // Maps IDs to indices in 'dense'
+        std::vector<int> indices; // Stores original IDs
+};
+
+// Usage
+// SparseArray<YourEntityType> entities;
+// entities.add(entityId, entity);
