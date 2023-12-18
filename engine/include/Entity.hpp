c02211931055dc720a0e9f9410dd5336e368b237
laetitia <laetitia.bousch@epitech.eu>
2023-12-18 09:28:21 +0100

    fix: refacto Entity.hpp

--- engine/include/Entity.hpp
+++ engine/include/Entity.hpp
@@ -13,23 +13,25 @@
 
 class IEntity {
     public:
+        template<typename T>
+
         virtual ~IEntity() = default;
         // The update method is called every frame
         virtual void update() = 0;
         virtual void render() = 0;
-        virtual void addComponent(std::unique_ptr<IComponent> component) = 0;
-        virtual void removeComponent(IComponent* component) = 0;
-        virtual std::vector<std::unique_ptr<IComponent>>& getComponents() = 0;
+        virtual void addComponent(std::unique_ptr<T> component) = 0;
+        virtual void removeComponent(T* component) = 0;
+        virtual std::vector<std::unique_ptr<T>>& getComponents() = 0;
 };
 
 class AbstractEntity : public IEntity {
     public:
         void update() override = 0;
         void render() override = 0;
-        void addComponent(std::unique_ptr<IComponent> component) override {
+        void addComponent(std::unique_ptr<T> component) override {
             components.push_back(std::move(component));
         }
-        void removeComponent(IComponent* component) override {
+        void removeComponent(T* component) override {
             for (auto it = components.begin(); it != components.end(); it++) {
                 if (it->get() == component) {
                     components.erase(it);
@@ -37,7 +39,7 @@ class AbstractEntity : public IEntity {
                 }
             }
         }
-        std::vector<std::unique_ptr<IComponent>>& getComponents() override {
+        std::vector<std::unique_ptr<T>>& getComponents() override {
             return components;
         }
     private:
