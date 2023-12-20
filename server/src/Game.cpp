1efd2e0629dd40f93ae1937c2e41a8d5c2ef7fa4
Léa GUILLEMARD <lea.guillemard@epitech.eu>
2023-12-20 01:48:41 +0100

    [ADD] tickrate in game loop, 66 tick by second

--- server/src/Game.cpp
+++ server/src/Game.cpp
@@ -20,10 +20,16 @@ void server::Game::run()
 {
     std::cout << "Game started" << std::endl;
     SystemManager manager;
+    auto tickDuration = std::chrono::microseconds(1000000) / 66;
+    auto nextTickTime = std::chrono::high_resolution_clock::now() + tickDuration;
 
     while (true) {
-        manager.update();
-        _tick++;
-        std::this_thread::sleep_for(std::chrono::milliseconds(_tickSpeed));
+        auto currentTime = std::chrono::high_resolution_clock::now();
+
+        if (currentTime >= nextTickTime) {
+            manager.update();
+            _tick++;
+            nextTickTime += tickDuration;
+        }
     }
 }
