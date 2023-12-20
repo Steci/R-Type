c983f1a41887810123b40915fc23262e1086fd3e
Léa GUILLEMARD <lea.guillemard@epitech.eu>
2023-12-20 02:18:23 +0100

    [ADD] generate a random port for client

--- client/src/ErrorHandling.cpp
+++ client/src/ErrorHandling.cpp
@@ -19,8 +19,8 @@ void ErrorHandling::help()
 
 std::vector<std::string> ErrorHandling::errorHandling(int argc, char **argv)
 {
-    std::string port = "";
-    std::string ipServer = "";
+    std::string serverPort = "";
+    std::string serverIP = "";
 
     for (int i = 1; i < argc; ++i) {
         if (std::strcmp(argv[i], "-h") == 0 || std::strcmp(argv[i], "--help") == 0) {
@@ -28,34 +28,34 @@ std::vector<std::string> ErrorHandling::errorHandling(int argc, char **argv)
             exit(0);
         } else if (std::strcmp(argv[i], "-i") == 0 || std::strcmp(argv[i], "--ip") == 0) {
             if (i + 1 < argc) {
-                if (checkIp(argv[i + 1]) == 84) {
+                if (checkIP(argv[i + 1]) == 84) {
                     std::cerr << "Invalid IP" << std::endl;
-                    return {"84", ipServer, port};
+                    return {"84", serverIP, serverPort};
                 }
-                ipServer = argv[i + 1];
+                serverIP = argv[i + 1];
                 ++i;
             } else {
                 std::cerr << "Invalid IP" << std::endl;
-                return {"84", ipServer, port};
+                return {"84", serverIP, serverPort};
             }
         } else if (std::strcmp(argv[i], "-p") == 0 || std::strcmp(argv[i], "--port") == 0) {
             if (i + 1 < argc) {
                 if (checkPort(argv[i + 1]) == 84) {
                     std::cerr << "Invalid port" << std::endl;
-                    return {"84", ipServer, port};
+                    return {"84", serverIP, serverPort};
                 }
-                port = argv[i + 1];
+                serverPort = argv[i + 1];
                 ++i;
             } else {
                 std::cerr << "Invalid port" << std::endl;
-                return {"84", ipServer, port};
+                return {"84", serverIP, serverPort};
             }
         } else {
             std::cerr << "Invalid argument" << std::endl;
-            return {"84", ipServer, port};
+            return {"84", serverIP, serverPort};
         }
     }
-    return {"0", ipServer, port};
+    return {"0", serverIP, serverPort};
 }
 
 int ErrorHandling::checkPort(char *port)
@@ -70,12 +70,12 @@ int ErrorHandling::checkPort(char *port)
     return 0;
 }
 
-int ErrorHandling::checkIp(char *ip)
+int ErrorHandling::checkIP(char *serverIP)
 {
     int i = 0;
 
-    while (ip[i] != '\0') {
-        if ((ip[i] < '0' || ip[i] > '9') && ip[i] != '.')
+    while (serverIP[i] != '\0') {
+        if ((serverIP[i] < '0' || serverIP[i] > '9') && serverIP[i] != '.')
             return 84;
         i++;
     }
