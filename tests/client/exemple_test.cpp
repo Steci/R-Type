08dde4298b53d947bbc144120e4d2e55ad5630cd
Malek Gatoufi <malek.gatoufi@epitech.eu>
2024-01-03 22:17:24 +0100

    [ADD] adding github action

---
+++ tests/client/exemple_test.cpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2023
+** R-Type
+** File description:
+** exemple_test.cpp
+*/
+
+// #include "your_code.h"  // inclure les fichiers nécessaires
+#include "gtest/gtest.h"  // inclure Google Test
+
+TEST(YourCodeTest, TestFunction1) {
+    // Écrire des assertions pour tester la fonction 1
+    ASSERT_EQ(YourCodeFunction1(2), 4);
+}
+
+TEST(YourCodeTest, TestFunction2) {
+    // Écrire des assertions pour tester la fonction 2
+    ASSERT_TRUE(YourCodeFunction2());
+}
